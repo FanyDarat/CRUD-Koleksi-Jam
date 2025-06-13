d865fe2666c6d361f19f200097b84f54479b285d
rafaelabednego123 <rafaelabednego123@gmail.com>
2025-06-13 14:52:56 +0000

    Membuat seeder

--- database/seeders/JamSeeder.php
+++ database/seeders/JamSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Jam;
 
 class JamSeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class JamSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $jams = [
+            [
+                'id_user' => null,
+                'name' => "Rolex Daytona Paul Newman",
+                'serialNumber' => "6239",
+                'imageUrl' => "gambar/jam1.jpg",
+            ],
+            [
+                'id_user' => null,
+                'name' => "Patek Philippe Nautilus 5711",
+                'serialNumber' => "5711/1A-010",
+                'imageUrl' => "gambar/jam2.jpg",
+            ],
+        ];
+        foreach($jams as $value) {
+            $jam = Jam::create($value);
+        }
     }
 }
